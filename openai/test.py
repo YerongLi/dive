043c4580ed8f21eea667bf24e71763b3339651bb
Yerong <yerong.li@outlook.com>
2023-04-01 02:12:50 -0500

    Show the key

--- openai/test.py
+++ openai/test.py
@@ -1,15 +1,17 @@
 # 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
 import openai
-openai.api_key = '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'
+import os
+openai.api_key = os.getenv("OPENAI_API_KEY")
+print(openai.api_key)
 prompt = "The massive explosions destroyed vehicles on a highway just outside the base at the Syrian port-city of Tartus, northwestern Syria. It is understood the first blast was a car bomb planetout outside the base. The second explosion was a suicide bomber who detonated his belt as people rushedto help those injured, AFP reported. What is attacker in the sentence: The massive explosions destroyed vehicles on a highway just outside the base at the Syrian port-city of Tartus, northwestern Syria?"
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=prompt,
-  temperature=0.7,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
-print(response)
+# response = openai.Completion.create(
+#   model="text-davinci-003",
+#   prompt=prompt,
+#   temperature=0.7,
+#   max_tokens=256,
+#   top_p=1,
+#   frequency_penalty=0,
+#   presence_penalty=0
+# )
+# print(response)
