dfcb1f0c293bc9c85fd4d720c6714b19b08382d0
Yerong <yerong.li@outlook.com>
2023-04-01 02:14:55 -0500

    Print the type of response

--- openai/test.py
+++ openai/test.py
@@ -9,9 +9,10 @@ response = openai.Completion.create(
   model="text-davinci-003",
   prompt=prompt,
   temperature=0.7,
-  max_tokens=256,
+  max_tokens=5,
   top_p=1,
   frequency_penalty=0,
   presence_penalty=0
 )
 print(response)
+print(type(response))
