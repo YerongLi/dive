523e82d79036572526d8ab52dc4a885466adfc2e
Yerong Li <yerong.li@outlook.com>
2024-04-26 04:13:48 -0500

    Upload

---
+++ solution_logisticReg/logistic.py
@@ -0,0 +1,76 @@
+import numpy as np
+from sklearn.metrics import confusion_matrix
+class LogisticRegression:
+    def __init__(self, learning_rate=0.001, n_iters=1000):
+        self.lr = learning_rate
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+        self.losses = []
+         
+    #Sigmoid method
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def compute_loss(self, y_true, y_pred):
+        # binary cross entropy
+        epsilon = 1e-9
+        y1 = y_true * np.log(y_pred + epsilon)
+        y2 = (1-y_true) * np.log(1 - y_pred + epsilon)
+        return -np.mean(y1 + y2)
+
+    def feed_forward(self,X):
+        z = np.dot(X, self.weights) + self.bias
+        A = self._sigmoid(z)
+        return A
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+
+        # init parameters
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        # gradient descent
+        for _ in range(self.n_iters):
+            A = self.feed_forward(X)
+            self.losses.append(self.compute_loss(y,A))
+            dz = A - y # derivative of sigmoid and bce X.T*(A-y)
+            # compute gradients
+            dw = (1 / n_samples) * np.dot(X.T, dz)
+            db = (1 / n_samples) * np.sum(dz)
+            # update parameters
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+            
+    def predict(self, X):
+        threshold = .5
+        y_hat = np.dot(X, self.weights) + self.bias
+        y_predicted = self._sigmoid(y_hat)
+        y_predicted_cls = [1 if i > threshold else 0 for i in y_predicted]
+        
+        return np.array(y_predicted_cls)
+
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+
+dataset = datasets.load_breast_cancer()
+X, y = dataset.data, dataset.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1234
+)
+
+regressor = LogisticRegression(learning_rate=0.0001, n_iters=1000)
+regressor.fit(X_train, y_train)
+predictions = regressor.predict(X_test)
+
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+# Assuming y_test are the true labels and predictions are the predicted labels from your model
+cm = confusion_matrix(y_test, predictions)
+accuracy = accuracy_score(y_test, predictions)
+
+print(f"Test accuracy: {accuracy:.3f}")
+# print("Confusion Matrix:\n", cm)
