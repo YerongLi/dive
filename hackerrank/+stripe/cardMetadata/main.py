976834940fee09c41cbd8e13dbdf223eb31734aa
Yerong Li <yerong.li@outlook.com>
2024-10-17 20:50:45 -0500

    Upload the results

--- hackerrank/+stripe/cardMetadata/main.py
+++ hackerrank/+stripe/cardMetadata/main.py
@@ -1,3 +1,115 @@
+def solutionD(inputs):
+	BIN = inputs[0]
+	n = inputs[1]
+	anslist = [x.split(',') for x in inputs[2:]]
+	anslist.sort(key = lambda x : [x[0], -int(x[1])])
+	first = 0
+	for i in range(n):
+		if anslist[first][1] < anslist[i][0]:
+			if anslist[first][2] != anslist[i][2]:
+				e = str(int(anslist[i][0]) - 1)
+				e = '0' * (10 - len(e)) + e
+				anslist[first][1] = e
+				first = i
+			else:
+				e = anslist[i][1]
+				anslist[first][1] = e
+				anslist[i][0] = None
+		if i == 0:
+			anslist[i][0] = '0' * 10
+		if i == n -1:
+			anslist[first][1] = '9' * 10
+	ans = []
+	for x in anslist:
+		if x[0]: ans.append(','.join([BIN+x[0], BIN+x[1], x[2]]))
+	return ans
+def solutionC(inputs):
+	BIN = inputs[0]
+	n = inputs[1]
+	anslist = [x.split(',') for x in inputs[2:]]
+	anslist.sort(key = lambda x : [x[0], -int(x[1])])
+	first = 0
+	for i in range(n):
+		if anslist[first][1] < anslist[i][0]:
+			e = str(int(anslist[i][0]) - 1)
+			e = '0' * (10 - len(e)) + e
+			anslist[first][1] = e
+			first = i
+		if i == 0:
+			anslist[i][0] = '0' * 10
+		if i == n -1:
+			anslist[first][1] = '9' * 10
+	ans = []
+	for x in anslist:
+		ans.append(','.join([BIN+x[0], BIN+x[1], x[2]]))
+	return ans
+
+def solutionA(inputs):
+	BIN= inputs[0]
+	n = inputs[1]
+	anslist = [x.split(',') for x in inputs[2:]]
+	anslist.sort()
+	anslist[0][0] = '0' * 10
+	anslist[n - 1][1] = '9' * 10
+	ans = []
+	for x in anslist:
+		ans.append(','.join([BIN+x[0], BIN+x[1],x[2]]))
+	# return x
+	return ans
+
+def solutionC1(inputs):
+	BIN = inputs[0]
+	N = inputs[1]
+	anslist = [x.split(',') for x in inputs[2:]]
+	anslist.sort(key = lambda x: [x[0], -int(x[1])])
+	first = 0
+	for i in range(N):
+		if i == 0:
+			anslist[i][0] = '0' * 10
+
+
+		if anslist[i][0] > anslist[first][1]:
+			e = str(int(anslist[i][0]) - 1)
+			e =  '0' * (10 - len(e)) + e
+			anslist[first][1] = e
+			first = i
+		if i == N - 1:
+			anslist[first][1] = '9' * 10
+	ans = []
+	for x in anslist:
+		ans.append(','.join([BIN+x[0],BIN+x[1],x[2]]))
+	return ans
+
+def solutionD1(inputs):
+	BIN = inputs[0]
+	N = inputs[1]
+	anslist = [x.split(',') for x in inputs[2:]]
+	anslist.sort(key = lambda x: [x[0], -int(x[1])])
+	first = 0
+	for i in range(N):
+		if i == 0:
+			anslist[i][0] = '0' * 10
+
+		# print(i, first, anslist[i][0] , anslist[first][1], anslist[i][0] > anslist[first][1])
+		if anslist[i][0] > anslist[first][1]:
+			print(anslist[i][2], anslist[first][2])
+			
+			if anslist[i][2] != anslist[first][2]:  
+				e = str(int(anslist[i][0]) - 1)
+				e =  '0' * (10 - len(e)) + e
+				anslist[first][1] = e
+				first = i
+			else:
+				e = anslist[i][1]
+				anslist[first][1] = e
+				anslist[i][0] = None
+		if i == N - 1:
+			anslist[first][1] = '9' * 10
+	ans = []
+	for x in anslist:
+		if x[0]: ans.append(','.join([BIN+x[0],BIN+x[1],x[2]]))
+	return ans
+
 def solutionZ(inputs):
 	BIN = inputs[0]
 	N = inputs[1]
@@ -6,20 +118,6 @@ def solutionZ(inputs):
 		start, end, t = inputs[i + 2].split(',')
 		ans.append(','.join([BIN+start, BIN+end, t]))
 	return ans
-
-def solutionA(inputs):
-	BIN = inputs[0]
-	N = inputs[1]
-	ans = []
-	li, hi = 0, 0
-	records = sorted(inputs[2:])
-	for i in range(N):
-		start, end, t = records[i].split(',')
-		if i == 0: start = '0' * 10
-		if i == N - 1: end = '9' * 10
-		ans.append(','.join([BIN+start, BIN+end, t]))
-	return ans
-
 
 def solutionB(inputs):
 	BIN = inputs[0]
@@ -43,63 +141,18 @@ def solutionB(inputs):
 	return ans
 
 
-def solutionC(inputs):
-	BIN = inputs[0]
-	N = inputs[1]
-	ans = []
-	li, hi = 0, 0
-	anslist = [record.split(',') for record in inputs[2:]]
-	anslist = [[record[0], record[1], record[2]] for record in anslist]
-	anslist.sort(key= lambda x: [x[0], -int(x[1])])
-	first = 0
-	for i in range(N):
-		if i == 0:
-			anslist[i][0] = '0' * 10
-		if i == N - 1:
-			anslist[first][1] = '9' * 10
-		if anslist[i][0] > anslist[first][1]:
-			s = str(int(anslist[i][0]) - 1)
-			ls = len(s)
-			s = '0' * (10 - ls)  + s
-			anslist[first][1] = s
-			first = i
-	for i in range(N):
-		ans .append(','.join([BIN+anslist[i][0], BIN+anslist[i][1], anslist[i][2]]))
-	return ans
 
-def solutionD(inputs):
-	BIN = inputs[0]
-	N = inputs[1]
-	ans = []
-	li, hi = 0, 0
-	anslist = [record.split(',') for record in inputs[2:]]
-	anslist = [[record[0], record[1], record[2]] for record in anslist]
-	anslist.sort(key= lambda x: [x[0], -int(x[1])])
-	first = 0
-	reslist= []
-	for i in range(N):
-		if i == 0:
-			anslist[i][0] = '0' * 10
-		if i == N - 1:
-			anslist[first][1] = '9' * 10
-		if anslist[i][0] > anslist[first][1]:
-			s = str(int(anslist[i][0]) - 1)
-			ls = len(s)
-			s = '0' * (10 - ls)  + s
-			anslist[first][1] = s
-			first = i
-
-	for i in range(N):
-		ans.append(','.join([BIN+anslist[i][0], BIN+anslist[i][1], anslist[i][2]]))
-	return ans
 print(' === PART 1')
 inputs = ['424242',
 2,
 '4000000000,8999999999,MASTERCARD',
 '1000000000,3999999999,VISA']
+expected = ['4242420000000000,4242423999999999,VISA', '4242424000000000,4242429999999999,MASTERCARD']
+
 ans = solutionA(inputs)
-for row in ans: print(row)
-print()
+assert expected == ans, "Assertion failed! The output doesn't match the expected result."
+
+# for row in ans: print(row)
 print(' === PART 2')
 
 inputs = ['424242',
@@ -107,8 +160,7 @@ inputs = ['424242',
 '0000000000,3700000000,MASTERCARD',
 '6100000000,9999999999,VISA']
 ans = solutionB(inputs)
-print(ans)
-for row in ans: print(row)
+# for row in ans: print(row)
 print()
 
 print(' === PART 3')
@@ -121,6 +173,7 @@ inputs = [
     '7000000000,8000000000,VISA'
 ]
 ans = solutionC(inputs)
+
 expected = ['4242420000000000,4242425999999999,VISA', 
 '4242422000000000,4242424000000000,CB', 
 '4242426000000000,4242429999999999,CB', 
@@ -128,4 +181,28 @@ expected = ['4242420000000000,4242425999999999,VISA',
 
 assert expected == ans, "Assertion failed! The output doesn't match the expected result."
 
+print(' === PART 4')
+inputs = [
+    '424242',  # card number
+    2,  # number of records
+    '0000000000,5999999999,VISA',
+    '6000000000,9999999999,VISA'
+]
+expected = ['4242420000000000,4242429999999999,VISA']
+ans = solutionD(inputs)
+# print(ans)
+assert expected == ans, "Assertion failed! The output doesn't match the expected result."
+inputs = [
+    '424242',  # card number
+    3,  # number of records
+    '1000000000,3999999999,VISA',
+    '5000000000,6999999999,VISA',
+    '8000000000,9999999999,MASTERCARD'
+]
+expected = ['4242420000000000,4242427999999999,VISA', 
+'4242428000000000,4242429999999999,MASTERCARD']
+
+ans = solutionD(inputs)
+
+assert expected == ans, "Assertion failed! The output doesn't match the expected result."
 print()
--- hackerrank/+stripe/intent/main.py
+++ hackerrank/+stripe/intent/main.py
@@ -1,7 +1,196 @@
 from collections import *
-def solutionA(inputs):
+def solutionD(inputs):
 	ma = OrderedDict()
 	mp = {}
+	ms = {}
+	ml = defaultdict(None)
+	mt = {}
+	for record in inputs:
+		line = record.split()
+		timestamp = int(line[0])
+		t = line[1]
+		if t == 'INIT':
+    		# "1 INIT m1 0 5",
+
+			act = line[2]
+			ma[act] = int(line[3])
+			if len(line) == 5:
+				ml[act] = int(line[4])
+		else:
+			p = line[2]
+			if t == 'CREATE':
+				_, _, _ , act, amt = line
+				amt = int(amt)
+				mp[p] = [act, amt]
+				ms[p] = t
+			elif t == 'ATTEMPT':
+				if ms[p] == 'CREATE':
+					ms[p] = t
+			elif t == 'SUCCEED':
+				if ms[p] == 'ATTEMPT':
+					act, amt = mp[p]
+					ma[act]+= amt
+					ms[p] = t
+					mt[p] = timestamp
+			elif t == 'FAIL':
+				if ms[p] == 'ATTEMPT':
+					ms[p] = 'CREATE'
+			elif t == 'UPDATE':
+				if ms[p] == 'ATTEMPT' or ms[p] == 'CREATE':
+					act, _ = mp[p]
+					mp[p] = [act, int(line[2])]
+			elif t == 'REFUND':
+				if ms[p] == 'SUCCEED':
+					act, amt = mp[p]
+					if not ml[act] or ml[act] + mt[p] >= timestamp:
+						ma[act]-= amt
+						ms[p] = t
+			else:
+				print(record, 'skip')
+	ans = []
+	for act, amt in ma.items():
+		ans.append(act+ ' '+str(amt))
+	return ans
+
+
+def solutionC(inputs):
+	ma = OrderedDict()
+	mp = {}
+	ms = {}
+	for record in inputs:
+		line = record.split()
+		t = line[0]
+		if t == 'INIT':
+			act = line[1]
+			ma[act] = int(line[2])
+		else:
+			p = line[1]
+			if t == 'CREATE':
+				_, _ , act, amt = line
+				amt = int(amt)
+				mp[p] = [act, amt]
+				ms[p] = t
+			elif t == 'ATTEMPT':
+				if ms[p] == 'CREATE':
+					ms[p] = t
+			elif t == 'SUCCEED':
+				if ms[p] == 'ATTEMPT':
+					act, amt = mp[p]
+					ma[act]+= amt
+					ms[p] = t
+			elif t == 'FAIL':
+				if ms[p] == 'ATTEMPT':
+					ms[p] = 'CREATE'
+			elif t == 'UPDATE':
+				if ms[p] == 'ATTEMPT' or ms[p] == 'CREATE':
+					act, _ = mp[p]
+					mp[p] = [act, int(line[2])]
+			elif t == 'REFUND':
+				if ms[p] == 'SUCCEED':
+					act, amt = mp[p]
+					ma[act]-= amt
+					ms[p] = t
+			else:
+				print(record, 'skip')
+	ans = []
+	for act, amt in ma.items():
+		ans.append(act+ ' '+str(amt))
+	return ans
+
+def solutionD1(inputs):
+	ma = OrderedDict()
+	ml = defaultdict(None)
+	mp = {}
+	ms = {}
+	mt = {}
+	for record in inputs:
+		line = record.split()
+		timestamp = int(line[0])
+
+		t = line[1]
+		if t == 'INIT':
+			# " time INIT m1 0 lll",
+			ma[line[2]] = int(line[3])
+			ml[line[2]] = int(line[4])
+		else:
+			p = line[2]
+			if t == 'CREATE':
+				# "2 CREATE p1 m1 100",
+
+				mp[p] = [line[3], int(line[4])]
+				ms[p] = t
+			elif t == 'ATTEMPT':
+				ms[p] = t
+			elif t == 'UPDATE':
+				if ms[p] == 'ATTEMPT':
+					act, _ = mp[p]
+					mp[p] = [act, int(line[2])]
+			elif t == 'SUCCEED':
+				ms[p] = t
+				act, amt = mp[p]
+				ma[act]+= amt
+				mt[p] = timestamp
+
+			elif t == 'REFUND':
+				# "11 REFUND p1",
+				act, _ = mp[p]
+				if ms[p] == 'SUCCEED' and mt[p] + ml[act] >= timestamp:
+					act, amt = mp[p]
+					ma[act]-= amt
+					ms[p] = t
+			elif t == 'FAIL':
+				ms[p] == 'CREATE'
+			else:
+				print(record)
+				print('skip')
+	ans = []
+	for act in ma:
+		ans.append(' '.join([act, str(ma[act])	]))
+	return ans
+
+def solutionC1(inputs):
+	ma = OrderedDict()
+	mp = {}
+	ms = {}
+	for record in inputs:
+
+		line = record.split()
+		t = line[0]
+		if t == 'INIT':
+			# "INIT m1 0",
+			ma[line[1]] = int(line[2])
+		else:
+			p = line[1]
+			if t == 'CREATE':
+				mp[p] = [line[2], int(line[3])]
+				ms[p] = t
+			elif t == 'ATTEMPT':
+				ms[p] = t
+			elif t == 'UPDATE':
+				if ms[p] == 'ATTEMPT':
+					act, _ = mp[p]
+					mp[p] = [act, int(line[2])]
+			elif t == 'SUCCEED':
+				ms[p] = t
+				act, amt = mp[p]
+				ma[act]+= amt
+			elif t == 'REFUND':
+				if ms[p] == 'SUCCEED':
+					act, amt = mp[p]
+					ma[act]-= amt
+					ms[p] = t
+			elif t == 'FAIL':
+				t == 'CREATE'
+			else:
+				print('skip')
+	ans = []
+	for act in ma:
+		ans.append(' '.join([act, str(ma[act])	]))
+	return ans
+
+def solutionA(inputs):
+	ma = OrderedDict() # [m, amt, timestamp]
+	mp = {} # [m, amt, timestamp]
 	ms = {}
 	ans = []
 	# INIT
@@ -52,69 +241,61 @@ def solutionB(inputs):
 		ans.append(' '.join([account, str(ma[account])]))
 	return ans
 
-def solutionC(inputs):
-	ma = OrderedDict()
-	mp = {}
-	ms = {}
-	ans = []
-	# INIT
-	# CREATE
-	# ATTEMPT
-	# SUCCESS
-	for record in inputs:
-		line = record.split()
-		if line[0] == 'INIT':
-			ma[line[1]] = int(line[2])
-		elif line[0] == 'CREATE':
-			mp[line[1]] = [line[2], int(line[3])]
-			ms[line[1]] = 'CREATE'
-		elif line[0] == 'UPDATE' and ms[line[1]] != 'SUCCEED':
-			account, _ = mp[line[1]]
-			mp[line[1]] = [account, int(line[2])]
-		elif line[0] == 'FAILED':
-			ms = [line[1]] = 'CREATE'
-		elif line[0] == 'REFUND':
-			if 
-		else:
-			ms[line[1]] = line[0]
-			if line[0] == 'SUCCEED': 
-				account, amt = mp[line[1]]
-				ma[account]+= amt
-	for account in ma:
-		ans.append(' '.join([account, str(ma[account])]))
-	return ans
+
 
 inputs = [
-    "INIT m1 0",
-    "INIT m2 10",
-    "CREATE p1 m1 50",
-    "ATTEMPT p1",
-    "SUCCEED p1",
-    "CREATE p2 m2 100",
-    "ATTEMPT p2"
+	"INIT m1 0",
+	"INIT m2 10",
+	"CREATE p1 m1 50",
+	"ATTEMPT p1",
+	"SUCCEED p1",
+	"CREATE p2 m2 100",
+	"ATTEMPT p2"
 ]
 expected = ['m1 50', 'm2 10']
 ans = solutionA(inputs)
 assert expected == ans, "Assertion failed! The output doesn't match the expected result."
 
 inputs = [
-    "INIT m1 0",
-    "CREATE p1 m1 50",
-    "UPDATE p1 100",
-    "ATTEMPT p1",
-    "SUCCEED p1"
+	"INIT m1 0",
+	"CREATE p1 m1 50",
+	"UPDATE p1 100",
+	"ATTEMPT p1",
+	"SUCCEED p1"
 ]
 expected = ['m1 100']
 ans = solutionB(inputs)
 assert expected == ans, "Assertion failed! The output doesn't match the expected result."
+print(' === PART 3')
 inputs = [
-    "INIT n1 0",
-    "CREATE p1 n1 50",
-    "ATTEMPT p1",
-    "FAIL p1",
-    "ATTEMPT p1",
-    "SUCCEED p1",
-    "CREATE p2 n1 100",
-    "ATTEMPT p2",
-    "REFUND p2"
+	"INIT n1 0",
+	"CREATE p1 n1 50",
+	"ATTEMPT p1",
+	"FAIL p1",
+	"ATTEMPT p1",
+	"SUCCEED p1",
+	"CREATE p2 n1 100",
+	"ATTEMPT p2",
+	"REFUND p2"
 ]
+expected = ['n1 50']
+ans = solutionC(inputs)
+print(ans)
+assert expected == ans, "Assertion failed! The output doesn't match the expected result."
+
+print(' === PART 4')
+inputs = [
+    "1 INIT m1 0 5",
+    "2 CREATE p1 m1 100",
+    "3 CREATE p2 m1 50",
+    "4 ATTEMPT p1",
+    "5 ATTEMPT p2",
+    "8 SUCCEED p1",
+    "10 SUCCEED p2",
+    "11 REFUND p1",
+    "16 REFUND p2"
+]
+expected = ['m1 50']
+
+ans = solutionD(inputs)
+assert expected == ans, "Assertion failed! The output doesn't match the expected result."
