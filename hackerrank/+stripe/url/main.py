32ee80e7754a775a752eb6e9c2d894e4726fe702
Yerong Li <yerong.li@outlook.com>
2025-02-26 14:50:36 -0600

    Upload

--- hackerrank/+stripe/url/main.py
+++ hackerrank/+stripe/url/main.py
@@ -1,28 +1,50 @@
 def short(part):
-    if len(part) <= 2:
-        return part
+    if len(part) <= 2: return part
     return f'{part[0]}{len(part)-2}{part[-1]}'
 
 def part1(url):
-    return '/'.join(['.'.join([short(part) for part in major.split('.')]) for major in url.split('/')])
-
+    l = [x.split('.') for x in url.split('/')]
+    ans = []
+    for x in l:
+        ans.append('.'.join([short(part) for part in x]))
+    res = '/'.join(ans)
+    return res
 def part2(url, m):
-    compressed = []
-    for major in url.split('/'):
-        minors = major.split('.')
-        if len(minors) > m:
-            merged = ''.join(minors[m-1:])
-            minors = minors[:m-1] + [merged]
-        compressed.append('.'.join(short(part) for part in minors))
-    return '/'.join(compressed)
+    l = [x.split('.') for x in url.split('/')]
+    ans = []
+    for x in l:
+        if len(x) > m:
+            x = x[:m-1] + [''.join(x[m-1:])]
+        ans.append('.'.join([short(part) for part in x]))
+    res = '/'.join(ans)
+    return res
 
 def part3(url, m):
-    minors = [minor for major in url.split('/') for minor in major.split('.')]
-    if len(minors) > m:
-        merged = ''.join(minors[m-1:])
-        minors = minors[:m-1] + [merged]
-    return '.'.join(short(part) for part in minors)
+    l = [x.split('.') for x in url.split('/')]
+    ans = []
+    length = [len(x) for x in l]
+    if sum(length) > m:
 
+        b, acc = 0,0
+        while b  < len(length):
+            acc+= length[b]
+            if acc >= m - 1: break
+        k = length[b]
+        r = acc - (m - 1)
+        merge = []
+        for i in range(b+1, len(l)):
+            merge+= l[i]
+        if r:
+            # split the l[b]
+            merge = l[b][k-r:] + merge
+            l[b] = l[b][:k-r] + [[''.join(merge)]]
+            l = l[:b+1]
+        else:
+            l = l[:b+1] + [[''.join(merge)]]
+    for x in l:
+        ans.append('.'.join([short(part) for part in x]))
+    res = '/'.join(ans)
+    return res
 def run_tests():
     assert part1("stripe.com/payments/checkout/customer.john.doe") == "s4e.c1m/p6s/c6t/c6r.j2n.d1e"
     assert part1("www.api.stripe.com/checkout") == "w1w.a1i.s4e.c1m/c6t"
@@ -31,8 +53,8 @@ def run_tests():
     assert part2("www.api.stripe.com/checkout", 3) == "w1w.a1i.s7m/c6t"
     assert part2("com.stripe/user/access.someapi.endpoint", 2) == "c1m.s4e/u2r/a4s.s13t"
     
-    assert part3("com.stripe/user/access.someapi.endpoint", 3) == "c1m.s4e.u23t"
-    assert part3("stripe.com/payments/checkout/customer.john.doe", 3) == "s4e.c1m.p19e"
+    assert part3("com.stripe/user/access.someapi.endpoint", 3) == "c1m.s4e/u23t"
+    assert part3("stripe.com/payments/checkout/customer.john.doe", 3) == "s4e.c1m/p29e"
     
     print("All test cases passed!")
 
